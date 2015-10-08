2073be7f59c5a9ba2577e145afec8fa139600b36
Curtis Wilcox <extra808@m.cognize.org>
2015-10-08 08:06:29 -0400

    write test file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,7 +24,7 @@ Route::get('/update', function () {
     chdir('..');
     $cwd = getcwd();
     $dump = json_encode($_POST);
-//    exec('echo "git pull\n"' ."$cwd\n"  .$dump .' >> storage/file');
+    exec('echo "git pull\n"' ."$cwd\n"  .$dump .' >> storage/file');
 
     return $cwd ."<br>". $dump;
 });
