6e2f14c19a8817956e5b05d50c6ad178dda9dec1
Curtis Wilcox <extra808@m.cognize.org>
2015-11-08 10:24:04 -0500

    add test seeder using model

--- database/seeds/BookTableSeeder.php
+++ database/seeds/BookTableSeeder.php
@@ -12,5 +12,14 @@ class BookTableSeeder extends Seeder
     public function run()
     {
         //
+        $book = new \Foobooks\Book();
+        $book->title = 'Harry Potter';
+        $book->author = 'JK Rowling';
+        $book->published = 1999;
+        $book->cover = 'http://p1.dwa15.cognize.org/photos/curtis-holding-macaw_sm.jpg';
+        $book->purchase_link = 'http://p1.dwa15.cognize.org/';
+        $book->page_count = 432;
+        $book->save(); // insert new book in table
+
     }
 }
